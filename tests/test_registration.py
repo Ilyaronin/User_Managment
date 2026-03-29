9cfac8810792efa2780c4e93e8a2bc79e394116f
Ilyaronin <novozhilov2007@internet.ru>
2026-03-29 19:09:19 +0300

    update test

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -59,10 +59,32 @@ def test_authenticate_noneexistent_user(setup_database, connection):
 
     result = authenticate_user("rtyey", "pass546")
     assert result == False
+
+def test_authenticate_success(setup_database):
+    add_user('ett', 'ett@mail.ru', 'pass12')
+
+    result = authenticate_user('ett', 'pass12')
+
+    assert result == True
+
+def test_display_users_output(setup_database, connection):
+    """Тест отображения списка пользователей."""
+    # Добавляем несколько пользователей
+    add_user('user1', 'user1@example.com', 'pass1')
+    add_user('user2', 'user2@example.com', 'pass2')
+    add_user('user3', 'user3@example.com', 'pass3')
+    
+    # Захватываем вывод функции
+    display_users()
+   
+    
+    # Проверяем, что вывод содержит информацию о пользователях
+    assert "Логин: user1, Электронная почта: user1@example.com" 
+    assert "Логин: user2, Электронная почта: user2@example.com" 
+    assert "Логин: user3, Электронная почта: user3@example.com" 
 # Возможные варианты тестов:
 """
 
 Тест успешной аутентификации пользователя.
-Тест аутентификации несуществующего пользователя.
-Тест отображения списка пользователей.
+
 """
